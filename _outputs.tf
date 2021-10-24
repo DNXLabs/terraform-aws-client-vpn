53ad2761b910acaaba675bbd319bc8ddc1da1efd
Will Thames <will@thames.id.au>
2021-10-24 15:00:09 +1000

    Create outputs for the client certificate and key

--- _outputs.tf
+++ _outputs.tf
@@ -4,3 +4,9 @@ output "security_group_id" {
 output "vpn_endpoint_id" {
   value = aws_ec2_client_vpn_endpoint.default.id
 }
+output "vpn_client_cert" {
+  value = tls_locally_signed_cert.root.cert_pem
+}
+output "vpn_client_key" {
+  value = tls_private_key.root.private_key_pem
+}
