9baafd7ec116020eed9ee37a5c7dc1a645c08b19
Todd Foley <todd.foley@stackedinvest.com>
2022-08-04 16:04:04 -0400

    fixed issue with cert resources not able to define encryption on readonly, added variable support for transport_protocol, session_timeout_hours, and login_banner_text; updated output with dns name and updated documentation to reflect the changes

--- _outputs.tf
+++ _outputs.tf
@@ -10,3 +10,6 @@ output "vpn_client_cert" {
 output "vpn_client_key" {
   value = tls_private_key.root.private_key_pem
 }
+output "vpn_dns_name" {
+  value = aws_ec2_client_vpn_endpoint.default.dns_name
+}
