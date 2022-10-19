79190659c391137b8721f70bb574fdf2f12f05f9
olialvesrobson <olialves.robson@gmail.com>
2022-10-19 00:22:16 +0000

    removing key_algorithm param

--- acm-certificate-server.tf
+++ acm-certificate-server.tf
@@ -3,7 +3,6 @@ resource "tls_private_key" "server" {
 }
 
 resource "tls_cert_request" "server" {
-  key_algorithm   = "RSA"
   private_key_pem = tls_private_key.server.private_key_pem
 
   subject {
@@ -14,7 +13,6 @@ resource "tls_cert_request" "server" {
 
 resource "tls_locally_signed_cert" "server" {
   cert_request_pem   = tls_cert_request.server.cert_request_pem
-  ca_key_algorithm   = "RSA"
   ca_private_key_pem = tls_private_key.ca.private_key_pem
   ca_cert_pem        = tls_self_signed_cert.ca.cert_pem
 
